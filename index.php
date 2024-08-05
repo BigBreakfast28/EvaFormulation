75da21add1abb4bf2ae3c2889e8e8d9fea63ebfd
Cameron <deancameron99@gmail.com>
2024-08-04 21:25:11 -0400

    Finishing the home page

--- index.php
+++ index.php
@@ -1,45 +1,162 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eva Formulatins - Home</title>
-    <link rel="stylesheet" href="./styles.css" >
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <head>
+        <title>Test Page</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-</head>
-<body>
-    <div class="container">
-        <ul class="navbar-nav ">
-            <li class="nav-item pt-4"><a class="nav-link" href="" id="">Contact</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Click Me</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Portfolio</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Reviews</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Services</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Home</a></li>
-        </ul>
-    </div>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="./styles.css">
+    </head>
 
-    <div class="advertisement">
-       <h1 class="banner1">Web design professional</h1>
-            <p class="description1">
-                This is going to be your <strong>ONE STOP SHOP</strong> for everything you need to get your business online.<br>
-                Having an online presence is mandatory for any professional. 
-            </p>
-        <h1 class="banner2">Expertise in design is mandatory</h1>
-    </div>
-f
-    <div>
-       <div>
-        <h1>Every site is custom built site</h1>
-        <p>We avoid using software to deliver you the best possible site for the cheapest price.</p>
-       </div>
-    </div>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-md bg-custom">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="./test.php"><img src="./public/EVATrans.png" style="width: 120px;"></a>
+                        <button class="navbar-toggler border-white bg-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+                            <ul class="navbar-nav ">
+                                <li class="nav-item"><a class="nav-link text-white pt-4">Home</a></li>
+                                <li class="nav-item"><a class="nav-link text-white pt-4">Services</a></li>
+                                <li class="nav-item"><a class="nav-link text-white pt-4">Mission</a></li>
+                                <li class="nav-item"><a class="nav-link text-white pt-4">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container-fluid cover">
+                <h1 class="display-1 text-white p-5 pop attention-grab">Seize the moment even if it is brief!</h1>
+                <p class="display-3 text-white p-5 attention-grab">This is just a bunch of filler text to see how the page looks.</p>
+                <h1 class="display-1 text-white p-5 pop attention-grab">I see that I can only have a padding max of "5" when setting margins and padding</h1>
+                <p class="display-3 text-white p-5 attention-grab">This is going to be an important lesson when building the rest of the page!</p>
+            </div>
 
-    
-</body>
+            <div class="container-fluid client-experience ">
+                <div class="row">
+                    <h1 class="pop text-indigo col">Excellent clientel experience is a priority</h1> 
+                    <p class="col">We strive to know our clients needs & expectations.<br>
+                        Understanding clients industry, target audience, and customer soltions are tools for business success.</p>
+                </div>
+            
+                <div class="offcanvas offcanvas-end" id="demo">
+                <div class="offcanvas-header">
+                    <h1 class="offcanvas-title text-indigo ">B.D.S.S is essential for customer success</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ol>
+                        <li class="pop text-blue">Business Goal</li>
+                        <li class="pop text-blue">Design</li>
+                        <li class="pop text-blue">Sustainability</li>
+                        <li class="pop text-blue">Support</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="container-fluid mt-3 mb-3">
+                <h3 class="pop display-4 process">Discover B.D.S.S process</h3>
+                <p>This is a system designed for you. </p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+                    Learn more
+                </button>
+            </div>
+        </div>
+
+           <div class="container-fluid bg-maroon website-type">
+                <h1 class="pop display-1 container process text-white bg-custom text-center">What We Offer</h1>
+                <div class="row">
+                    <div class="col-sm-4 p-4">
+                        <h1 class="pop text-blue display-4">Portfolio websites</h1>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title ">Photography Portfolio website</h3>
+                                    <p class="card-text">Creating a beautiful portfolio website to display to clients is essential for attracting clients.</p>
+                                    <a class="card-link" href="www.kodakkevin.com">Take a look</a>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="col-sm-4 p-4">
+                        <h1 class="pop text-blue display-4">Business websites</h1>
+                        <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title">Nonprofit Organizations & more</h3>
+                                    <p class="card-text">Businesses that strive to empower the community are essential to development.</p>
+                                    <a class="card-link" >Take a look</a>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="col-sm-4 p-4">
+                        <h1 class="pop text-blue display-4">Ecommerce Websites</h1>
+                        <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title">Take your business online</h3>
+                                    <p class="card-text">
+                                        Benefits of a ecommerce business include: cost reduction, brand expansion, global reach, & unique shopping experience.
+                                        <i>This service is still under construction and will be avaliable to the public soon!</i>
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+           </div>
+        </main>
+        <footer>
+            <div class="container-fluid mt-5">
+                   <div class="container-fluid row">
+                   <div class="col-sm-3 foot-img"></div>
+                        <div class="col-sm-3 ">
+                            <h3 class="display-4 text-indigo pt-4">Location</h3>
+                            <ul>
+                                <li>Atlanta,GA</li>
+                                <li>Address-info</li>
+                                <li>Email-info</li>
+                                <li>Possible Phone-info</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-sm-3 icon-list">
+                            <h5 class="display-4 text-indigo pt-4">Follow EVA</h5>
+                            <div>
+                            <ul class="row ">
+                                <li class="col pt-3">Facebook Logo</li>
+                                <li class="col pt-3">Instagram Logo</li>
+                                <li class="col pt-3">TikTok Logo</li>
+                                <li class="col pt-3">Youtube Logo</li>
+                            </ul>
+                        </div>
+                        </div>
+                   </div>
+            </div>
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
