ce2ecf162525f8048805de404b5c1d686de25cd5
Cameron <deancameron99@gmail.com>
2024-08-06 13:08:24 -0400

    Creating contact form

--- mission.php
+++ mission.php
@@ -1,16 +1,15 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Test Page</title>
+        <title>
+            EVA-contact
+        </title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
         <!-- Bootstrap CSS v5.2.1 -->
         <link
@@ -19,12 +18,16 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
         <link rel="stylesheet" href="./styles.css">
     </head>
 
     <body>
         <header>
-            <nav class="navbar navbar-expand-md bg-custom">
+        <nav class="navbar navbar-expand-md bg-custom">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="./test.php"><img src="./public/EVATrans.png" style="width: 120px;"></a>
                         <button class="navbar-toggler border-white bg-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
@@ -32,7 +35,7 @@
                         </button>
                         <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
                             <ul class="navbar-nav ">
-                                <li class="nav-item"><a class="nav-link text-white pt-4" href="./index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link text-white pt-4" href="./index.php">Home</a></li>
                                 <li class="nav-item"><a class="nav-link text-white pt-4" href="./services.php">Services</a></li>
                                 <li class="nav-item"><a class="nav-link text-white pt-4" href="./mission.php">Mission</a></li>
                                 <li class="nav-item"><a class="nav-link text-white pt-4" href="./contact.php">Contact</a></li>
@@ -41,85 +44,9 @@
                     </div>
             </nav>
         </header>
-        <main>
-            <div class="container-fluid cover">
-                <h1 class="display-1 p-5 pop attention-grab">Web development professional</h1>
-                <p class="display-3 p-5 subattention-grab">This is just a bunch of filler text to see how the page looks.</p>
-                <h1 class="display-1 p-5 pop attention-grab">I see that I can only have a padding max of "5" when setting margins and padding</h1>
-                <p class="display-3 p-5 attention-grab">This is going to be an important lesson when building the rest of the page!</p>
-            </div>
-
-            <div class="container-fluid client-experience ">
-                <div class="row">
-                    <h1 class="pop text-indigo col">Excellent clientel experience is a priority</h1> 
-                    <p class="col">We strive to know our clients needs & expectations.<br>
-                        Understanding clients industry, target audience, and customer soltions are tools for business success.</p>
-                </div>
-            
-                <div class="offcanvas offcanvas-end" id="demo">
-                <div class="offcanvas-header">
-                    <h1 class="offcanvas-title text-indigo ">B.D.S.S is essential for customer success</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ol>
-                        <li class="pop text-blue">Business Goal</li>
-                        <li class="pop text-blue">Design</li>
-                        <li class="pop text-blue">Sustainability</li>
-                        <li class="pop text-blue">Support</li>
-                    </ol>
-                </div>
-            </div>
-
-            <div class="container-fluid mt-3 mb-3">
-                <h3 class="pop display-4 process">Discover B.D.S.S process</h3>
-                <p>This is a system designed for you. </p>
-                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
-                    Learn more
-                </button>
-            </div>
-        </div>
-
-           <div class="container-fluid bg-maroon website-type">
-                <h1 class="pop display-1 container process text-white bg-custom text-center">What We Offer</h1>
-                <div class="row">
-                    <div class="col-sm-4 p-4">
-                        <h1 class="pop text-blue display-4">Portfolio websites</h1>
-                            <div class="card">
-                                <div class="card-body">
-                                    <h3 class="card-title ">Photography Portfolio website</h3>
-                                    <p class="card-text">Creating a beautiful portfolio website to display to clients is essential for attracting clients.</p>
-                                    <a class="card-link" href="www.kodakkevin.com">Take a look</a>
-                                </div>
-                            </div>
-                    </div>
-                    <div class="col-sm-4 p-4">
-                        <h1 class="pop text-blue display-4">Business websites</h1>
-                        <div class="card">
-                                <div class="card-body">
-                                    <h3 class="card-title">Nonprofit Organizations & more</h3>
-                                    <p class="card-text">Businesses that strive to empower the community are essential to development.</p>
-                                    <a class="card-link" >Take a look</a>
-                                </div>
-                            </div>
-                    </div>
-                    <div class="col-sm-4 p-4">
-                        <h1 class="pop text-blue display-4">Ecommerce Websites</h1>
-                        <div class="card">
-                                <div class="card-body">
-                                    <h3 class="card-title">Take your business online</h3>
-                                    <p class="card-text">
-                                        Benefits of a ecommerce business include: cost reduction, brand expansion, global reach, & unique shopping experience.
-                                        <i>This service is still under construction and will be avaliable to the public soon!</i>
-                                    </p>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-           </div>
-        </main>
+        <main></main>
         <footer>
-            <div class="container-fluid mt-5">
+        <div class="container-fluid mt-5">
                    <div class="container-fluid row">
                    <div class="col-sm-3 foot-img"></div>
                         <div class="col-sm-3 ">
